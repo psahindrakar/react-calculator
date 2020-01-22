99e64721f9210907f0b84910f1b96e399e02c770
Pratik Sahindrakar <pratik.sahindrakar@figmd.com>
2020-01-23 00:15:29 +0530

    Created Calculator display basic component

---
+++ src/containers/Calculator/components/Display/Display.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Display.css';
+
+function Display() {
+    return (
+        <div className="display">
+            1
+        </div>
+    );
+}
+
+export default Display;
